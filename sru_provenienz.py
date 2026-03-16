425de427cd6e21d6226f5a4406b940a9e7d65185
Roman Kuhn <96392831+r0man-ist@users.noreply.github.com>
2026-03-16 16:22:36 +0100

    remove unneccessary imports

--- sru_provenienz.py
+++ sru_provenienz.py
@@ -1,31 +1,27 @@
 import marimo
 
-__generated_with = "0.16.4"
+__generated_with = "0.20.4"
 app = marimo.App(width="medium")
 
 
 @app.cell
 def _(mo):
-    mo.md(
-        """
+    mo.md("""
     # Provenienz-Explorer
     ## Ein Notebook zur Abfrage, Exploration und Visualisierung von Provenienzdaten im Verbundkatalog [k10plus](https://www.bszgbv.de/services/k10plus/)
-    """
-    )
+    """)
     return
 
 
 @app.cell
 def _(mo):
-    mo.md(
-        """
+    mo.md("""
     Die Provenienzdaten werden über die [SRU-Schnittstelle](https://wiki.k10plus.de/spaces/K10PLUS/pages/27361342/SRU) des
     k10plus abgerufen.
 
     Dieses Notebook ist ein [Marimo](https://docs.marimo.io/)-Notebook – es kann als App oder in der Code-Ansicht ausgeführt werden.
     Um zwischen den Ansichten zu wechseln, drücken Sie **Strg + .** oder drücken Sie den button "Toggle app view" unten rechts.
-    """
-    )
+    """)
     return
 
 
@@ -50,6 +46,7 @@ def _():
     import json
     import math
     from plotly.subplots import make_subplots
+
     return (
         Any,
         Counter,
@@ -118,8 +115,7 @@ def _(mo):
 
 @app.cell
 def _(mo):
-    mo.md(
-        """
+    mo.md("""
     ### Suchanfrage
     Für die Suche über die SRU-Schnittstelle des k10plus (Basis-Url: [https://sru.k10plus.de/opac-de-627](https://sru.k10plus.de/opac-de-627)) muss die Suchanfrage u.a. spezifizieren, welcher/welche Suchschlüssel mit welchen Werten abgefragt werden sollen.
 
@@ -128,8 +124,7 @@ def _(mo):
     Eine Übersicht der Indexschlüssel findet sich [hier](https://format.k10plus.de/k10plushelp.pl?cmd=idx_s). Für das Folgende werden drei Suchschlüssel verwendet: **pica.tit** für die Suche in **Titeln**, **pica.prk** für die Suche nach **Provenienzinformationen als Schlagwort** und **pica.prp** für eine **Phrasensuche** in einem normierten Index.
 
     Alternativ können Sie einen Suchstring frei eingeben; dabei sind auch Operatoren wie *and* oder *not* möglich.
-    """
-    )
+    """)
     return
 
 
@@ -181,6 +176,7 @@ def _(
 
 
         return number_of_records 
+
     return (get_nr_of_records,)
 
 
@@ -272,6 +268,7 @@ def _(
                 break
 
         return all_records
+
     return (query_sru,)
 
 
@@ -345,6 +342,7 @@ def func_parse(ET, NS, etree, pd, unicodedata):
                     "Jahr": year,
                     "URL": URL}
         return meta_dict
+
     return (parse_record,)
 
 
@@ -471,18 +469,17 @@ def _(ET, NS, etree, unicodedata):
             row_data["URL"] = f"https://opac.k10plus.de/DB=2.299/PPNSET?PPN={record_id}&PRS=HOL&INDEXSET=21"
 
         return data
+
     return (parse_ex,)
 
 
 @app.cell
 def _(all_ex, mo, unique_exemplare):
-    mo.md(
-        f"""
+    mo.md(f"""
     Das Ergebnisset enthält **{len(all_ex)} Aussagen** zur Provenienz, die sich auf **{len(unique_exemplare)} Exemplare** beziehen.
 
     Wenn Sie eine Titelsuche ausgeführt haben, kann es sein, dass keine Provenienzinformationen ausgegeben werden – in diesem Fall sind schlicht zu den gefundenen Titeln keine Provenienzinformationen hinterlegt.
-    """
-    )
+    """)
     return
 
 
@@ -759,6 +756,7 @@ def _(ISIL_SRU_BASE, NS, cache, etree, requests):
             return name
         else:
             return inst_id
+
     return (fetch_institution_name,)
 
 
@@ -925,6 +923,7 @@ def _(Any, Counter, Dict, Iterable, List, Tuple, switch_discard_nn):
             val.append(cnt)
 
         return labels, src, tgt, val
+
     return (provenance_to_sankey_arrays,)
 
 
@@ -987,7 +986,7 @@ def _(go, make_subplots, math):
             row_size = total
             n_rows = 1
         else:
-        
+
             n_rows = math.ceil(total / max_row_size)
             row_size = math.ceil(total / n_rows)  # equal chunks, each <= max_row_size
 
@@ -1036,6 +1035,7 @@ def _(go, make_subplots, math):
         )
 
         return fig
+
     return (plot_year_heatmap,)
 
 
