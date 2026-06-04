ef07761cc779dba4f9baa5da55bad41458956182
Roman Kuhn <96392831+r0man-ist@users.noreply.github.com>
2026-06-04 15:34:13 +0200

    Update sru_provenienz_embed.py

--- sru_provenienz_embed.py
+++ sru_provenienz_embed.py
@@ -607,7 +607,9 @@ def _(df_ex, pd, query, re, shlex, unicodedata):
             parts = str(q).split()
 
         seq = []
-        field_eq_re = re.compile(r'^([\w\.\-]+)=(.*)$')  # match field=rest
+
+        # match field=rest
+        field_eq_re = re.compile(r'^([\w\.\-]+)=(.*)$')  
         for p in parts:
             # case-insensitive operator detection
             if p.casefold() in ("and", "or", "not"):
